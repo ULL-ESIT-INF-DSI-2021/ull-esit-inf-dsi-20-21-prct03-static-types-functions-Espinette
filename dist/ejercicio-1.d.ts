79e51b68daa94df6a431732108422c8224b65883
Alberto Rios de la Rosa <alu0101235929@ull.edu.es>
2021-03-01 19:21:12 +0000

    Estructura creada

---
+++ dist/ejercicio-1.d.ts
@@ -0,0 +1,2 @@
+declare function add(firstNum: any, secondNum: any): any;
+declare const mySum: any;
---
+++ src/ejercicio-1.ts
@@ -0,0 +1,16 @@
+// Ejercicio 1 - Años bisiestos
+
+/* function sum(year) {
+  return year + year;
+}
+
+const bisiestos = sum(1900);
+console.log(`bisiestos = ${bisiestos}`);
+ */
+
+function add(firstNum, secondNum) {
+  return firstNum + secondNum;
+}
+
+const mySum = add(1, 7);
+console.log(`mySum = ${mySum}`);
