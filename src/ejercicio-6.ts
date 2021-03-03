34ef9d3ac3bc93ea666e779f412ed8a2c863bab1
Alberto Rios de la Rosa <alu0101235929@ull.edu.es>
2021-03-03 19:01:57 +0000

    Arreglado fallos de ejercicio 3 y 5

---
+++ src/ejercicio-6.ts
@@ -0,0 +1,16 @@
+function isValidISBN(cadena: string) {
+  
+}
+
+
+const isbnwithX: string = ``;
+const solisbnwithX: string = chuckNorris(isbnwithX);
+console.log(`ISBN con X, se espera que sea válido(true): ${solisbnwithX}`);
+
+const isbn: string = ``;
+const solisbn: string = chuckNorris(isbn);
+console.log(`ISBN, se espera que sea válido(true): ${solisbn}`);
+
+const isbnerror: string = ``;
+const solisbnerror: string = chuckNorris(isbnerror);
+console.log(`ISBN, se espera que no sea válido(false): ${solisbnerror}`);
