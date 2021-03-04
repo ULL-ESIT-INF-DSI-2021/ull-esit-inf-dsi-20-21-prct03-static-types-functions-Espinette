efb6a22b15d02279cee9f0552ceb18769f8a4cd5
Alberto Rios de la Rosa <alu0101235929@ull.edu.es>
2021-03-04 21:01:22 +0000

    Ejercicio 9 resuelto

---
+++ dist/ejercicio-9.d.ts
@@ -0,0 +1,1 @@
+declare function dañoPokemon(tipoPok1: string, tipoPok2: string, capAtaq1: number, capAtaq2: number): number;
---
+++ src/ejercicio-9.ts
@@ -0,0 +1,49 @@
+// Ejercicio 9 - Entrenador Pokemon
+
+function dañoPokemon(tipoPok1: string, tipoPok2: string,
+    capAtaq1: number, capAtaq2: number) {
+  let efectividad: number = 0;
+  let daño: number = 0;
+  switch (tipoPok1) {
+    case `fuego`:
+      if (tipoPok2 = `hierba`) {
+        efectividad = 2;
+      } else if (tipoPok2 = `electrico`) {
+        efectividad = 1;
+      } else {
+        efectividad = 0.5;
+      }
+      break;
+    case `hierba`:
+      if (tipoPok2 = `agua`) {
+        efectividad = 2;
+      } else if (tipoPok2 = `electrico`) {
+        efectividad = 1;
+      } else {
+        efectividad = 0.5;
+      }
+      break;
+    case `electrico`:
+      if (tipoPok2 = `agua`) {
+        efectividad = 2;
+      } else if ((tipoPok2 = `fuego`) || (tipoPok2 = `hierba`)) {
+        efectividad = 1;
+      } else {
+        efectividad = 0.5;
+      }
+      break;
+    case `agua`:
+      if (tipoPok2 = `fuego`) {
+        efectividad = 2;
+      } else {
+        efectividad = 0.5;
+      }
+      break;
+  }
+  daño = 50 * (capAtaq1 / capAtaq2) * efectividad;
+  return daño;
+}
+
+
+console.log(`${dañoPokemon("fuego", "hierba", 1000, 200)}`);
+console.log(`${dañoPokemon("agua", "electrico", 320, 640)}`);
