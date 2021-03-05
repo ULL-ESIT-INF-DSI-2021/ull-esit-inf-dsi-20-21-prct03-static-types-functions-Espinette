2f86677c0196575a9aa2fad80dcecabb141d96cd
Alberto Rios de la Rosa <alu0101235929@ull.edu.es>
2021-03-05 16:00:41 +0000

    Ejercicio 10 realizado

---
+++ dist/ejercicio-10.d.ts
@@ -0,0 +1,1 @@
+declare function isValidUsername(nombreUser: string): boolean;
---
+++ src/ejercicio-10.ts
@@ -0,0 +1,21 @@
+function isValidUsername(nombreUser: string) {
+  const tam: number = nombreUser.length;
+  if ((tam >= 4) && (tam <= 30)) {
+    if ((nombreUser[0] != "_") && (nombreUser[tam - 1] != "_")) {
+      if ((/[a-z]/.test(nombreUser)) && (/[A-Z]/.test(nombreUser)) &&
+          (/[$_-]/.test(nombreUser)) && (/\d/.test(nombreUser))) {
+        if ((!/[a-z][a-z][a-z]/.test(nombreUser)) &&
+            (!/[A-Z][A-Z][A-Z]/.test(nombreUser)) &&
+              (!/[$_-][$_-][$_-]/.test(nombreUser)) &&
+               (!/\d\d\d/.test(nombreUser))) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+
+console.log(`Usuario Alu01$bn valido? : ${isValidUsername("Alu01$bn")}`);
+console.log(
+    `Usuario u__hello$122__ valido? : ${isValidUsername("u__hello$122__")}`);
